d34981eded67eec1e343537674c13d478a9acab0
Fatih EGE <ifatxh@gmail.com>
2023-02-01 21:53:22 +0300

    Add favicon link

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,7 @@ export default () => {
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true}/>
+                <link rel="shortcut icon" href="/favicon.ico"/>
                 <title>Fatih EGE</title>
             </Head>
             <div className={styles.container}>
